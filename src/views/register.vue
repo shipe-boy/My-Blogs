a43f9009bbc89e437797dd9fbd544e520bb0356b
shipe <1370027889@qq.com>
2019-05-04 15:53:48 +0800

    My blogs

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,58 +1,64 @@
 <!--  -->
 <template>
-    <div>
-        <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
-            <el-form-item label="昵称" prop="name" :required="true">
-                <el-input style="width:50%;" v-model="ruleForm2.name"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="pass" :required="true">
-                <el-input style="width:50%;" type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="确认密码" prop="checkPass" :required="true">
-                <el-input style="width:50%;" type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="邮箱" prop="email">
-                <el-input style="width:50%;" type="email" v-model="ruleForm2.email" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="手机号码" prop="phone">
-                <el-input style="width:50%;" type="phone" v-model="ruleForm2.phone" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-                <el-button @click="resetForm('ruleForm2')">重置</el-button>
-            </el-form-item>
-        </el-form>
-        <el-row>
-            <el-button @click='esc'>取消</el-button>
-        </el-row>
-        <el-button style="display:none" :plain="true" @click="open2">成功</el-button>
-        <el-button style="display:none" :plain="true" @click="open4">错误</el-button>
-    </div>
+  <div>
+    <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="昵称" prop="name" :required="true">
+        <el-input style="width:50%;" v-model.trim="ruleForm2.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass" :required="true">
+        <el-input style="width:50%;" type="password" v-model.trim="ruleForm2.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass" :required="true">
+        <el-input style="width:50%;" type="password" v-model.trim="ruleForm2.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input style="width:50%;" type="email" v-model.trim="ruleForm2.email" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号码" prop="phone">
+        <el-input style="width:50%;" type="phone" v-model.trim="ruleForm2.phone" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        <el-button @click="resetForm('ruleForm2')">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <el-row>
+      <el-button @click='esc'>取消</el-button>
+    </el-row>
+    <el-button style="display:none" :plain="true" @click="open2">成功</el-button>
+    <el-button style="display:none" :plain="true" @click="open4">错误</el-button>
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
+  export default {
+    data() {
       let checkUser = (rule, value, callback) => {
-        if (value.trim() === '') {
+        if (value === '') {
           callback(new Error('请输入用户名'));
+        } else if (value.length < 4 || value.length > 12) {
+          callback(new Error('用户名6~12位'));
         } else {
-            this.$ajax.post('/users/isuser',{username:value})
-                    .then(res =>{
-                        if(res.data.status == 0){
-                            callback(new Error('用户名已存在'))
-                        }else{
-                          callback();
-                        }
-                    }).catch(err =>{
-                        callback(new Error('用户名已存在'));
-                    })
-          
+          this.$ajax.post('/users/isuser', {
+              username: value
+            })
+            .then(res => {
+              if (res.data.status == 0) {
+                callback(new Error('用户名已存在'))
+              } else {
+                callback();
+              }
+            }).catch(err => {
+              callback(new Error('用户名已存在'));
+            })
+
         }
       }
       let validatePass = (rule, value, callback) => {
-        if (value.trim() === '') {
+        if (value === '') {
           callback(new Error('请输入密码'));
+        } else if (value.length < 6 || value.length > 12) {
+          callback(new Error('密码6~12位'));
         } else {
           if (this.ruleForm2.checkPass !== '') {
             this.$refs.ruleForm2.validateField('checkPass');
@@ -61,7 +67,7 @@ export default {
         }
       };
       let validatePass2 = (rule, value, callback) => {
-        if (value.trim() === '') {
+        if (value === '') {
           callback(new Error('请再次输入密码'));
         } else if (value !== this.ruleForm2.pass) {
           callback(new Error('两次输入密码不一致!'));
@@ -69,27 +75,27 @@ export default {
           callback();
         }
       };
-      let phone = (rule, value, callback) =>{
-        if(value.trim() !== "") {
-            if(!(/^1[34578]\d{9}$/.test(value))){
-              callback(new Error('手机号码有误'));
-            }else{
-              callback();
-            }
-        }else{
+      let phone = (rule, value, callback) => {
+        if (value.trim() !== "") {
+          if (!(/^1[34578]\d{9}$/.test(value))) {
+            callback(new Error('手机号码有误'));
+          } else {
+            callback();
+          }
+        } else {
           callback();
-        } 
+        }
       };
-      let email =  (rule, value, callback) =>{
-        if(value.trim() !== "") {
-            if(!(/^(\w-.)+@(\w-?)+(.\w{2,})+$/.test(value))){
-              callback(new Error('邮箱有误'));
-            }else{
-              callback();
-            }
-        }else{
+      let email = (rule, value, callback) => {
+        if (value.trim() !== "") {
+          if (!(/^(\w-.)+@(\w-?)+(.\w{2,})+$/.test(value))) {
+            callback(new Error('邮箱有误'));
+          } else {
+            callback();
+          }
+        } else {
           callback();
-        } 
+        }
       };
       return {
         ruleForm2: {
@@ -100,21 +106,26 @@ export default {
           phone: ''
         },
         rules2: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          name: [
-            { validator: checkUser, trigger: 'blur' }
-          ],
-          email: [
-            {validator: email, trigger: 'blur'}
-          ],
-          phone: [
-            {validator: phone, trigger: 'blur'}
-          ]
+          pass: [{
+            validator: validatePass,
+            trigger: 'blur'
+          }],
+          checkPass: [{
+            validator: validatePass2,
+            trigger: 'blur'
+          }],
+          name: [{
+            validator: checkUser,
+            trigger: 'blur'
+          }],
+          email: [{
+            validator: email,
+            trigger: 'blur'
+          }],
+          phone: [{
+            validator: phone,
+            trigger: 'blur'
+          }]
         },
         msg: '',
       };
@@ -124,29 +135,29 @@ export default {
         this.$refs[formName].validate((valid) => {
           if (valid) {
             this.$ajax.post('/users/register', this.ruleForm2)
-                      .then(res => { //进入数据库检测后
-                        // console.log(res)
-                        if(res.data.status === 0){
-                          this.open2()
-                          
-                          this.$store.commit('userName', res.data.result.username)
-                          this.$store.commit('userFace', res.data.result.userface)
-                          this.$emit('on-close')
-                          this.ruleForm2.name = ""
-                          this.ruleForm2.pass = ""
-                          this.ruleForm2.checkPass = ""
-                          this.ruleForm2.email = ""
-                          this.ruleForm2.phone = ""
-                        }else{
-                          this.msg = res.data.msg
-                          this.open4()
-                        }
-                      })
-                      .catch(err => {
-                        console.log(err)
-                        this.msg = '注册失败'
-                        this.open4()
-                      })
+              .then(res => { //进入数据库检测后
+                // console.log(res)
+                if (res.data.status === 0) {
+                  this.open2()
+
+                  this.$store.commit('userName', res.data.result.username)
+                  this.$store.commit('userFace', res.data.result.userface)
+                  this.$emit('on-close')
+                  this.ruleForm2.name = ""
+                  this.ruleForm2.pass = ""
+                  this.ruleForm2.checkPass = ""
+                  this.ruleForm2.email = ""
+                  this.ruleForm2.phone = ""
+                } else {
+                  this.msg = res.data.msg
+                  this.open4()
+                }
+              })
+              .catch(err => {
+                console.log(err)
+                this.msg = '注册失败'
+                this.open4()
+              })
           } else {
             return false;
           }
@@ -155,7 +166,7 @@ export default {
       resetForm(formName) {
         this.$refs[formName].resetFields();
       },
-      esc(){
+      esc() {
         this.$emit('on-close')
       },
       open2() {
@@ -167,9 +178,9 @@ export default {
       open4() {
         this.$message.error(`${this.msg}`);
       }
-      
+
     }
-}
+  }
 
 </script>
 <style scoped>
